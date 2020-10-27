232228da09a088112a7c717b12e2b9903f0900f3
kogriffith <kevin.o.griffith@gmail.com>
2020-10-26 23:30:41 -0700

    update seed information

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Conference.create(name: 'National Football Conference')
+Conference.create(name: 'American Football Conference')
